8a4585572bd6c99d9b82854fda5d40207143270d
Oleh Prokopenko <olehprokopenko1@gmail.com>
2025-11-08 18:35:31 +0200

    correct mistakes 02

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const inputs = document.querySelectorAll('.field-text');
+const inputs = document.querySelectorAll('form .field-text');
 
 inputs.forEach((input) => {
   const label = document.createElement('label');
